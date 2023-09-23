8940dac81242ebce500426ff133a615bdf8c4a9b
sfmoraa <sfx-sjtu@sjtu.edu.cn>
2023-09-23 23:00:27 +0800

    sfx

--- README.md
+++
@@ -1,2 +0,0 @@
-# SJTUhelper
-A helpful website that grants SJTU students easy access to trending information and campus-related updates.
--- app01/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-print("apps running")
-
-class App01Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app01'
